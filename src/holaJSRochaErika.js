ab713f674a1cce99fae8cdd24ca5f1b8068283c2
rochaerika775-cell <rochaerika775@gmail.com>
2026-04-23 15:57:35 -0300

    Agrego archivo JS - Erika Rocha

---
+++ src/holaJSRochaErika.js
@@ -0,0 +1,10 @@
+//no no usar
+var nombre = "Gustavo";
+//trabajar con let y const
+let apellido = "Rocha";
+//const TEXTO = "Hola Mundo, segun ";
+const TEXTO = `Hola Mundo, segun ${nombre} ${apellido} `;
+//const TEXTO = `Hola Mundo, segun ${nombre} ${apellido} `;
+
+//const.log ("Hola Mundo, segun" ${nombre} ${apellido} `;
+console.log(TEXTO + nombre + apellido);
