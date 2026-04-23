2f9cf09f2878d30227f899b19c6364a08bdbcc0e
Mauro-27 <mauroxcampos.27@gmail.com>
2026-04-23 15:34:27 -0300

    primer commit Mauro Campos

---
+++ src/holaJSCamposMauro.js
@@ -0,0 +1,8 @@
+const nombre = "Mauro";
+let apellido = "Campos";
+// const TEXTO = "Hola Mundo, segun";
+const TEXTO = ` Hola Mundo, segun ${nombre} ${apellido} `;
+// const TEXTO = " Hola Mundo, segun ${nombre} ${apellido} "; 
+
+console.log(TEXTO + nombre + apellido);
+
