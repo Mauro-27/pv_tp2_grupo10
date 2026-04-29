35175e41e379a6c56749d5caea6c5726b5a271ee
rochaerika775-cell <rochaerika775@gmail.com>
2026-04-29 13:10:23 -0300

    Agrego ejercicio 4 - Erika Rocha

---
+++ src/ejercicio4.js
@@ -0,0 +1,8 @@
+const lado1 = 10; // cm
+const lado2 = 5;  // cm
+
+const area = lado1 * lado2;
+const perimetro = 2 * (lado1 + lado2);
+
+console.log(`Área: ${area} cm²`);
+console.log(`Perímetro: ${perimetro} cm`);
