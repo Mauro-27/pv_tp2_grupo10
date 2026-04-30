7b6ac2a3ba12260378155c38b62cdb7493ba3e8a
lucasbarr13 <lucas.btos07@gmail.com>
2026-04-30 19:35:17 -0300

    Agrego ejercicio 16, 20 y servicio personal

--- src/js/ejercicio20.js
+++ src/js/ejercicio20.js
@@ -3,3 +3,5 @@ import { servicioMauroCampos } from "../services/serviceMauroCampos";
 import { serviceGabrielChauque } from "../services/serviceGabrielChauque";
 
 import { servicioAlexanderRobles } from "../services/serviceAlexanderRobles";
+
+import { servicioLucasBarrientos } from "../services/serviceLucasBarrientos.js";
